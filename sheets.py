5b6db4a881ecc66b41636082197fef1d3276f57d
Steven Dunn <thevgm01@gmail.com>
2021-08-27 17:10:22 -0500

    Numerous improvements
    Fixes for plurality always being Plural
    Tracks the number of components added and won't return if nothing was added
    Ending in a vowel is determined at the start
    Capitalizes each component

--- sheets.py
+++ sheets.py
@@ -47,72 +47,93 @@ def getRandomEntry(*cols):
 
 def generateNickname():
     components = []
-    subjectPlurality = ""
+    componentsAdded = 0
+    subjectPlurality = -1
+    subjectEndsInVowel = False
+
     # Any subject, don't proceed until one is found
     while not components:
         coords = getRandomCoords('M', 'N', 'O')
         entry = data[coords[0]][coords[1]]
         if entry:
             components.append(entry)
-            if coords[1] == ord('M'):
+            if coords[1] == ord('M') - ord('A'):
                 subjectPlurality = Plurality.ANY
-            elif coords[1] == ord('N'):
+            elif coords[1] == ord('N') - ord('A'):
                 subjectPlurality = Plurality.SINGULAR
-            else:
+            elif coords[1] == ord('O') - ord('A'):
                 subjectPlurality = Plurality.PLURAL
-    print(subjectPlurality)
-    # Get pre-subject
+            subjectEndsInVowel = isVowel(entry[-1])
+
+    # Pre Subject
     entry = getRandomEntry('L')
     if entry:
         components[0] = entry + components[0]
+        componentsAdded += 1
+
     # Descriptors
     descriptors = []
     while True:
         coords = getRandomCoords('I', 'J')
         entry = data[coords[0]][coords[1]]
         if entry and coords[1] == ord('I'):
+            # TODO Track components added here
             entry = getRandomEntry('H') + entry + getRandomEntry('K')
             descriptors.append(entry)
+            componentsAdded += 1
         else:
             if entry:
                 descriptors.append(entry)
+                componentsAdded += 1
             break
     components = descriptors + components
+
     # Curse
     entry = getRandomEntry('G')
     if entry:
         components.insert(0, entry)
+        componentsAdded += 1
+
     # Quantity
     entry = getRandomEntry('F')
     if entry and subjectPlurality != Plurality.SINGULAR:
         components.insert(0, entry)
+        componentsAdded += 1
+
     # Pre Everything
     if subjectPlurality == Plurality.SINGULAR:
         entry = getRandomEntry('D', 'E')
+        componentsAdded += 1
     else:
         entry = getRandomEntry('D')
+        componentsAdded += 1
     if entry:
         components.insert(0, entry)
+        componentsAdded += 1
+
     # Post Subject
-    # TODO FIX
-    lastIsVowel = isVowel(components[-1][-1])
     if subjectPlurality != Plurality.PLURAL:
         coords = getRandomCoords('P', 'R')
     else:
         coords = getRandomCoords('P', 'Q')
     entry = data[coords[0]][coords[1]]
     if entry:
-        if lastIsVowel and coords[1] == ord('Q') and data[coords[0]][coords[1] + 1]:
+        if subjectEndsInVowel and coords[1] == ord('Q') and data[coords[0]][coords[1] + 1]:
             entry = data[coords[0]][coords[1] + 1]
         components[-1] = components[-1] + entry
+        componentsAdded += 1
+
     # Modifier
     entry = getRandomEntry('T')
     if entry:
         components.append(entry)
+        componentsAdded += 1
+
     # Post Everything
     entry = getRandomEntry('U')
     if entry:
         components.append(entry)
+        componentsAdded += 1
 
     # TODO Replace all double spaces with single spaces
     # TODO Make sure the same word can't appear more than once
@@ -120,6 +141,18 @@ def generateNickname():
     # TODO Fix bias with selecting individual items from different columns
     #      (calculate a ratio of filled-to-unfilled cells for each column)
     # TODO Make the | separator work
+
+    # Abort if nothing new was added
+    # TODO: Make this a low chance instead of automatic?
+    if componentsAdded <= 1:
+        return ""
+
+    # Capitalize
+    i = 0
+    while i < len(components):
+        components[i] = components[i][0].upper() + components[i][1:]
+        i += 1
+
     result = " ".join(components)
     return result
 
@@ -145,5 +178,6 @@ numCols = len(data[0]) - colOffset
 i = 0
 while i < 20:
     nickname = generateNickname()
-    print(nickname)
-    i += 1
+    if nickname:
+        print(nickname)
+        i += 1
