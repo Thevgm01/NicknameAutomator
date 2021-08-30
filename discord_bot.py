594984dd4b20bb5c80fb7dbbf6f2a36885fb69ac
Steven Dunn <thevgm01@gmail.com>
2021-08-30 12:07:31 -0500

    Can force generated nicknames to have certain text

--- discord_bot.py
+++ discord_bot.py
@@ -16,17 +16,29 @@ async def on_ready():
 @bot.command(name='name', aliases=["n", "nick", "nickname"], help='Generates a random nickname')
 async def post_nickname(ctx, *args):
     num = 1
+    force = ""
+    attempts = 0
+    limit = 10000
     for arg in args:
         if arg[0] == 'x':
             try:
                 num = int(arg[1:])
             except ValueError:
                 pass
+        else:
+            force = arg.lower()
     response = ""
     i = 0
     while i < num:
-        response += nickname_generator.generate_nickname() + "\n"
-        i += 1
+        cur = nickname_generator.generate_nickname()
+        if force == "" or force in cur.lower():
+            response += cur + '\n'
+            i += 1
+        elif response == "":  # Track failures if nothing has come back successful yet
+            if attempts >= limit:
+                await ctx.send("I generated %s nicknames, but none of them had the string \"%s\"." % (limit, force))
+                return
+            attempts += 1
     await ctx.send(response)
 
 
